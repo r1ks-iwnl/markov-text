f8813e9f41bbef744a4d732d9431d711bd7985e6
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2023-11-14 19:13:49 +0200

    e

--- markov.js
+++ markov.js
@@ -1,12 +1,16 @@
 function markov(text) {
     let words = text.split(" ");
     let markovChain = new Map();// TODO: get a key to have more than 1 possible value
+    let result = new Map();
     for(i = 0; i < words.length; i++ ) {
         if(words[i + 2] != undefined) {
             markovChain.set(`${words[i]} ${words[i+1]}`, words[i+2]);
         }
     }
-    console.log(markovChain)
-    //console.log(Array.from(markovChain.keys())[1])
+    for(i = 1; i <= markovChain.size; i++) {
+        
+    }
+    console.log(markovChain);
+    console.log(Array.from(markovChain.keys())[0]);
 }
 markov("a a a b c")
