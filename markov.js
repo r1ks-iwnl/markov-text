3392455ae0e3fda917cd8674440c757312af0f19
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2024-02-21 19:46:18 +0200

    back in black

--- markov.js
+++ markov.js
@@ -1,16 +1,26 @@
+function prob(percent) {
+	if (Math.random() * 100 < percent) {
+		return true
+	} else return false;
+}
+
 function markov(text) {
     let words = text.split(" ");
     let markovChain = new Map();// TODO: get a key to have more than 1 possible value
-    let result = new Map();
+    let result = [];
     for(i = 0; i < words.length; i++ ) {
         if(words[i + 2] != undefined) {
             markovChain.set(`${words[i]} ${words[i+1]}`, words[i+2]);
         }
     }
-    for(i = 1; i <= markovChain.size; i++) {
-        
+    result.push(Array.from(markovChain.keys())[0]);
+    for(i = 0; i < markovChain.size; i++) {
+        if(prob(50)) {
+            result.push(markovChain.get(Array.from(markovChain.keys())[i]));
+
+        }
     }
     console.log(markovChain);
-    console.log(Array.from(markovChain.keys())[0]);
+    console.log(result);
 }
-markov("a a a b c")
+markov("a a b c")
