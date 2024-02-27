116ea29e49a5bb4ba2f51744fb51a7c6b1dd17d2
r1ks-iwnl <69760936+r1ks-iwnl@users.noreply.github.com>
2024-02-27 19:38:00 +0200

    result can now pick a value from an array

--- markov.js
+++ markov.js
@@ -11,7 +11,7 @@ function markov(text) {
     for(i = 0; i < words.length; i++ ) {
         if(words[i + 2] != undefined) {
             if(markovChain.has(`${words[i]} ${words[i+1]}`)) { //so keys don't get overwritten values
-                const flatNewValue = [words[i+2], markovChain.get(`${words[i]} ${words[i+1]}`)].flat()
+                const flatNewValue = [words[i+2], markovChain.get(`${words[i]} ${words[i+1]}`)].flat();
                 markovChain.set(
                     `${words[i]} ${words[i+1]}`,
                     flatNewValue
@@ -20,12 +20,18 @@ function markov(text) {
             else markovChain.set(`${words[i]} ${words[i+1]}`, words[i+2]);
         }
     }
-    result.push(Array.from(markovChain.keys())[0]); //add support for keys with multiple values
+    result.push(Array.from(markovChain.keys())[0]);
     for(i = 0; i < markovChain.size; i++) {
-        if(prob(50)) {
+        const valueLength = markovChain.get(Array.from(markovChain.keys())[i]).length;
+        if(markovChain.get(Array.from(markovChain.keys())[i]).length > 1) {//pick a value for keys with multiple values
+            result.push(markovChain.get(
+                Array.from(markovChain.keys())[i])
+                [Math.floor(valueLength * Math.random())]
+            ); //could use some variables instead of all this
+        }  
+        else if(prob(50)) {
             result.push(markovChain.get(Array.from(markovChain.keys())[i]));
-
-        }
+        } // i have no clue if the RNG is random enough
     }
     console.log(markovChain);
     console.log(result);
